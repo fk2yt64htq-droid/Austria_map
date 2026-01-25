c70b8a214e3d14c5b39cf7e5827c69d02ca9ef72
fk2yt64htq-droid <fk2yt64htq@privaterelay.appleid.com>
2026-01-25 13:15:49 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import time
 import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Спільна пам'ять для всіх водіїв
+# Сховище для статистики
 db = {}
 
-@app.route('/stats', methods=['GET'])
+@app.route('/stats')
 def get_stats():
-    return jsonify(db)
+    # Відправляємо статистику та поточний час сервера (now)
+    return jsonify({
+        "stats": db, 
+        "now": int(time.time())
+    })
 
 @app.route('/update', methods=['POST'])
 def update():
@@ -19,13 +24,14 @@ def update():
     status = data.get('status')
     
     if p_id not in db:
-        db[p_id] = {'green': 0, 'red': 0}
+        db[p_id] = {'green': 0, 'red': 0, 'time': 0}
     
     if status in ['green', 'red']:
         db[p_id][status] += 1
+        db[p_id]['time'] = int(time.time()) # Фіксуємо час оновлення
         
-    return jsonify(db[p_id])
+    return jsonify({"success": True})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
